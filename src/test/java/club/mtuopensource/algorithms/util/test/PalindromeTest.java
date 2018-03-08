d2356ff3031098a8afffbc7be8ab12709bb861a2
Austin Walhof <ajwgeek@gmail.com>
2018-03-07 21:48:17 -0500

    Added IsPalindrome algorithm and tests

---
+++ src/test/java/club/mtuopensource/algorithms/util/test/PalindromeTest.java
@@ -0,0 +1,25 @@
+package club.mtuopensource.algorithms.util.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import club.mtuopensource.algorithms.util.Palindrome;
+
+public class PalindromeTest {
+	
+	@Test
+	public void testPalindromes() {
+		assertTrue(Palindrome.isPalindrome("a"));
+		assertTrue(Palindrome.isPalindrome("abba"));
+		assertTrue(Palindrome.isPalindrome("radar"));
+		assertTrue(Palindrome.isPalindrome("kayak"));
+	}
+	
+	@Test
+	public void testNonPalindromes() {
+		assertFalse(Palindrome.isPalindrome("at"));
+		assertFalse(Palindrome.isPalindrome("cat"));
+		assertFalse(Palindrome.isPalindrome("dog"));
+	}
+}
