9ebc068d90a1123a183f7dad7083794a6e35441c
Steven Brylinski <sjbrylin@mtu.edu>
2018-10-23 19:01:07 -0400

    Added tests to the linear search for python

--- python/linear_search.py
+++ python/linear_search.py
@@ -1,5 +1,11 @@
 def linear_search(array, key):
-    for i in range 0,len(array):
+    for i in range (0, len(array)):
         if array[i] == key:
             return i
     return -1
+
+if __name__ == "__main__":
+    array = [4,3,2,1,5,7,4]
+    print (linear_search(array, 4)) #exists
+    print (linear_search(array,9)) #does not exist in array
+    print (linear_search(array,5)) #also exists
