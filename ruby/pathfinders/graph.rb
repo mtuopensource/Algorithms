792ca106a511c89a5e0b511be677b967b6360063
Ryan Philipps <raphilip@mtu.edu>
2018-10-23 23:14:04 -0400

    Created a simple graph and edge class for the path finder algorithms to use

--- ruby/pathfinders/graph.rb
+++ ruby/pathfinders/graph.rb
@@ -1,2 +1,16 @@
 class Graph
+  def initialize
+    @vertices = Array.new
+    @edges = Array.new
+  end
+
+  def add_edge(u, v, weight)
+    raise('Vertex not in graph') if @vertices.find(u) == nil or @vertices.find(v) == nil
+    @edges.insert Edge.new u, v, weight
+  end
+
+  def add_vertex(v)
+    @vertices.insert v
+  end
+
 end
