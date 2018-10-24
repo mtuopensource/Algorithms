a8b6cd3725754de2eb9c388854fcfeb6028802c4
Austin Walhof <ajwgeek@gmail.com>
2018-10-24 00:32:01 -0400

    Added documentation to AutomatedSortTest

--- java/src/test/java/club/mtuopensource/algorithms/test/sort/AutomatedSortTest.java
+++ java/src/test/java/club/mtuopensource/algorithms/test/sort/AutomatedSortTest.java
@@ -20,14 +20,21 @@ public class AutomatedSortTest {
     private static final int SEED = 0xdeadbeef;
     private static final int NUMBER_OF_ELEMENTS = 99;
 
+    /**
+     * Sort
+     * Fills two ArrayLists with {@link AutomatedSortTest#NUMBER_OF_ELEMENTS} random Integers.
+     * ArrayList one is sorted using the given algorithm, while ArrayList two is sorted using Java's built-in sorting
+     * routine. Asserts that ArrayList one equals ArrayList two.
+     * @param algorithm currently being tested for correctness.
+     */
     @ParameterizedTest
     @MethodSource("getSorters")
     public void sort(Class<? extends ISort> algorithm) {
         try {
             Supplier<Stream<Integer>> data = () -> new Random(SEED).ints(NUMBER_OF_ELEMENTS).boxed();
             ArrayList<Integer> a = (ArrayList) data.get().collect(Collectors.toList());
-            ArrayList<Integer> b = (ArrayList) data.get().sorted().collect(Collectors.toList());
-            ISort<Integer> sorter = algorithm.newInstance();
+            ArrayList<Integer> b = (ArrayList) data.get().sorted().collect(Collectors.toList()); // Sorted
+            ISort<Integer> sorter = algorithm.newInstance(); // TODO: Deprecated. Can this be replaced with Constructor?
             sorter.sort(a);
             assertEquals(a, b);
         } catch(InstantiationException e) {
@@ -37,6 +44,10 @@ public class AutomatedSortTest {
         }
     }
 
+    /**
+     * GetSorters
+     * @return Stream of ISort implementations. Used as parameters in the Sort Test.
+     */
     private static Stream<Arguments> getSorters() {
         Reflections reflections = new Reflections("club.mtuopensource.algorithms");
         Set<Class<? extends ISort>> sorters = reflections.getSubTypesOf(ISort.class);
