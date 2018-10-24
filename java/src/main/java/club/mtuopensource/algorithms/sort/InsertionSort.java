ec6e3b2d35bac5f42980478f89afc178ba112e0a
Isaac Smith <isaacns98@gmail.com>
2018-10-23 22:09:08 -0400

    Implemented insertion sort
    * Implemented Insertion Sort algorithm and test cases
    * Add Eclipse files to .gitignore

---
+++ java/src/main/java/club/mtuopensource/algorithms/sort/InsertionSort.java
@@ -0,0 +1,27 @@
+package club.mtuopensource.algorithms.sort;
+import java.util.ArrayList;
+
+/**
+ * Sorts an array using the MergeSort algorithm
+ * @author Isaac Smith
+ * Last Modified: 10 - 23 - 18
+ */
+public class InsertionSort<E extends Comparable<E>> implements ISort<E> {
+
+	/**
+	 * Implemented sort() method from package
+	 */
+	public void sort(ArrayList<E> array) {
+		for (int i = 1; i < array.size(); i++) {
+			E key = array.get(i);
+			int j = i-1;
+			// Move elements that are greater than key to right by one
+			while( j >= 0 && array.get(j).compareTo(key) > 0) {
+				array.set(j+1, array.get(j--));
+			}
+			array.set(j+1, key);
+		}
+	}
+
+
+}
---
+++ java/src/test/java/club/mtuopensource/algorithms/test/sort/InsertionSortTest.java
@@ -0,0 +1,75 @@
+package club.mtuopensource.algorithms.test.sort;
+
+import club.mtuopensource.algorithms.sort.ISort;
+import club.mtuopensource.algorithms.sort.InsertionSort;
+import org.junit.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class InsertionSortTest{
+	ISort<Integer> sorter;
+	ArrayList<Integer> expectedData;
+	ArrayList<Integer> data;
+
+	@Before
+	public void setup() {
+		sorter = new InsertionSort<Integer>();
+	}
+
+	@Test
+	public void test1() {
+		data = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+		expectedData = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+		sorter.sort(data);
+		assertEquals(expectedData, data);
+	}
+
+	@Test
+	public void test2() {
+		data = new ArrayList<>(Arrays.asList(10,9,8,7,6,5,4,3,2,1));
+		expectedData = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+		sorter.sort(data);
+		assertEquals(expectedData, data);
+	}
+
+	@Test
+	public void test3() {
+		data = new ArrayList<>(Arrays.asList(1,10,2,9,3,8,4,5,7,6));
+		expectedData = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+		sorter.sort(data);
+		assertEquals(expectedData, data);
+	}
+
+	@Test
+	public void test4() {
+		data = new ArrayList<>(Arrays.asList(10,9,9,7,6,6,6,3,2,1));
+		expectedData = new ArrayList<>(Arrays.asList(1,2,3,6,6,6,7,9,9,10));
+		sorter.sort(data);
+		assertEquals(expectedData, data);
+	}
+
+	@Test
+	public void test5() {
+		data = new ArrayList<>();
+		expectedData = new ArrayList<>();
+
+        int a = 5; //a must be 4c+1
+        int b = 3; //b must be odd
+        int m = 1024;
+
+        int x = 10; //the first number;
+        data.add(x);
+		for (int i=1;i<1024;i++ ) {
+			x = ( a * x + b ) % m;
+			data.add(x);
+		}
+
+		for (int i=0;i<1024;i++ ) {
+			expectedData.add(i);
+		}
+		sorter.sort(data);
+		assertEquals(expectedData, data);
+	}
+}
