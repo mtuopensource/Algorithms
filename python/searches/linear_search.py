6f703a80743d1573b251d7fb1312642132309314
Austin Walhof <ajwgeek@gmail.com>
2018-10-27 12:49:26 -0400

    Create search tests

--- python/searches/linear_search.py
+++ python/searches/linear_search.py
@@ -3,9 +3,3 @@ def linear_search(array, key):
         if array[i] == key:
             return i
     return -1
-
-if __name__ == "__main__":
-    array = [4, 3, 2, 1, 5, 7, 4]
-    print (linear_search(array, 4)) #exists
-    print (linear_search(array, 9)) #does not exist in array
-    print (linear_search(array, 5)) #also exists
